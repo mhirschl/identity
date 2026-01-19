c574865d47642644b5a3fff9ae62c4d73058f6a8
Matthew Hirschland <hirschland@google.com>
2026-01-18 18:53:29 -0700

    Fix: Push index.html and update PWA cache to v2

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'identity-habit-v1';
+const CACHE_NAME = 'identity-habit-v2';
 const ASSETS = [
     './',
     './index.html',
     './css/style.css',
     './js/app.js',
+    './js/sync.js', // Added sync.js to cache
     './js/store.js',
     './manifest.json',
     './assets/icon-192.png',
@@ -11,10 +12,26 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Force new service worker to take over immediately
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(ASSETS);
         })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Clearing old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim()) // Take control of all pages immediately
     );
 });
 
